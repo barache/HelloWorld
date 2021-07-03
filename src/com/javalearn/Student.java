f487a42388f56e82c84572cd176a23162d5c8479
Barache <kbarache@gmail.com>
2021-07-03 12:33:22 -0500

    Create student class

---
+++ src/com/javalearn/Student.java
@@ -0,0 +1,60 @@
+package com.javalearn;
+
+public class Student {
+    private int studentCode;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    public Student() {
+    }
+
+    public Student(int studentCode, String firstName, String lastName, String email, String phone) {
+        this.studentCode = studentCode;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getStudentCode() {
+        return studentCode;
+    }
+
+    public void setStudentCode(int studentCode) {
+        this.studentCode = studentCode;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
